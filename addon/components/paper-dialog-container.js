c6def144dbb7690c69d90650c84faa5411fbc014
Egor <hoboman313@users.noreply.github.com>
2018-06-03 23:45:20 -0700

    iOS friendly way to trigger 'outsideClicked' in the paper-dialog-container

--- addon/components/paper-dialog-container.js
+++ addon/components/paper-dialog-container.js
@@ -11,15 +11,33 @@ import { invokeAction } from 'ember-invoke-action';
 export default Component.extend({
   classNames: ['md-dialog-container'],
 
-  mouseDown(ev) {
-    this._sourceEl = ev.target;
-  },
-
-  mouseUp(ev) {
+  onMouseUp(ev) {
     if (this._sourceEl === this.element && ev.target === this.element) {
       ev.stopPropagation();
       ev.preventDefault();
       invokeAction(this, 'outsideClicked');
     }
+  },
+
+  onMouseDown(ev) {
+    this._sourceEl = ev.target;
+  },
+
+  mouseDown(ev) {
+    this.onMouseDown(ev);
+  },
+
+  mouseUp(ev) {
+    this.onMouseUp(ev);
+  },
+
+  // needed for iOS
+  touchStart(ev) {
+    this.onMouseDown(ev);
+  },
+
+  // needed for iOS
+  touchEnd(ev) {
+    this.onMouseUp(ev);
   }
 });
